cc242d02bf430b28bc66389fc99ac25ca6187ea4
SATT21-05-08-Son-Nguyen <96458008+SATT21-05-08-Son-Nguyen@users.noreply.github.com>
2021-12-24 10:59:46 +0700

    Add files via upload

---
+++ src/main/java/Common/Common/Utilities.java
@@ -0,0 +1,8 @@
+package Common.Common;
+
+public class Utilities {
+
+    public static String getprojectPath(){
+        return System.getProperty("user.dir");
+    }
+}
---
+++ src/main/java/Common/Constant/Constant.java
@@ -0,0 +1,11 @@
+package Common.Constant;
+
+import org.openqa.selenium.WebDriver;
+
+public class Constant {
+
+        public static WebDriver WEBDRIVER;
+        public static final String RAILWAY_URL = "http://www.railway.somee.com/Page/HomePage.cshtml";
+        public static final String USERNAME = "aaa@mail.com";
+        public static final String PASSWORD = "123456789";
+}
